d1731a36e593c8ee4accaff9257b605d23a56f20
PTRRR <pietro.alberti@bluewin.ch>
2021-09-30 16:01:02 +0200

    Use fetch

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,7 +6,9 @@
 export default {
   async asyncData({ params, $http }) {
     const { language } = params
-    const data = await $http.$get(language ? `/${language}` : '/')
+    const data = await fetch(
+      `https://api-pplex.gavillet-cie.com${language ? `/${language}` : '/'}`
+    ).then((res) => res.json())
     return data
   },
 }
