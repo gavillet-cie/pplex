dd318d982f1c136379136595e1d604ee78951d40
PTRRR <pietro.alberti@bluewin.ch>
2021-10-19 12:25:19 +0200

    Implement cache

--- backend/site/ready.php
+++ backend/site/ready.php
@@ -4,4 +4,12 @@ $this->addHookAfter('Pages::saveReady', function (HookEvent $event) {
   if ($page->template == "lawyer") {
     $page->title = $page->lawyer_first_name . ' ' . strtoupper($page->lawyer_last_name);
   }
+
+  $http = new WireHttp();
+  $response = $http->get("http://reverse-proxy:8080/api/purge-cache");
+  if ($response !== false) {
+    $this->message("Nginx cache cleared!");
+  } else {
+    $this->message("HTTP request failed: " . $http->getError());
+  }
 });
---
+++ reverse-proxy/bin/purge-cache.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This is a comment!
+rm -rf data/nginx/cache/*
+mkdir -p /data/nginx/cache
