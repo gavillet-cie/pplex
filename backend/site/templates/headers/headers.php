b2f592282f1dd04876be8513b241fc74879504af
PTRRR <pietro.alberti@bluewin.ch>
2021-10-29 11:41:47 +0200

    Update config

--- backend/site/templates/headers/headers.php
+++ backend/site/templates/headers/headers.php
@@ -1,14 +1,14 @@
 <?php
-  require_once $config->paths->templates.'/api/rest.php';
+require_once $config->paths->templates . '/api/rest.php';
 
-  $JSONheader = Rest\Header::mimeType('json');
-  header($JSONheader);
-  header('Strict-Transport-Security: max-age=0;', true);
-  header('Access-Control-Allow-Origin: *', true);
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS', true);
-  header('Access-Control-Allow-Headers: Content-Type', true);
-  header_remove('Pragma');
-  header_remove('pragma');
-  header_remove('cache-control');
-  header_remove('Cache-Control');
+$JSONheader = Rest\Header::mimeType('json');
+header($JSONheader);
+header('Strict-Transport-Security: max-age=0;', true);
+header('Access-Control-Allow-Origin: https://pplex.gavillet-cie.com', true);
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET', true);
+header('Access-Control-Allow-Headers: Content-Type', true);
+header_remove('Pragma');
+header_remove('pragma');
+header_remove('cache-control');
+header_remove('Cache-Control');
