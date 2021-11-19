5327cc1b665bb61d5681930611dec1f5a009c860
PTRRR <pietro.alberti@bluewin.ch>
2021-11-19 11:02:15 +0100

    Update cms hooks

--- backend/site/ready.php
+++ backend/site/ready.php
@@ -1,10 +1,31 @@
 <?php
 $this->addHookAfter('Pages::saveReady', function (HookEvent $event) {
   $page = $event->arguments("page");
+
+  $defaultName = $page->localName('default');
+
+  if ($page->template->name != 'home') {
+    wire('pages')->setOutputFormatting(false);
+    foreach (wire('languages') as $lang) {
+      if ($lang->isDefault()) continue;
+      $page->set("name$lang", $defaultName);
+    }
+  }
+
   if ($page->template == "lawyer") {
     $page->title = $page->lawyer_first_name . ' ' . strtoupper($page->lawyer_last_name);
   }
 
+  $http = new WireHttp();
+  $response = $http->get("http://reverse-proxy:8080/api/purge-cache");
+  if ($response !== false) {
+    $this->message("Nginx cache cleared!");
+  } else {
+    $this->message("HTTP request failed: " . $http->getError());
+  }
+});
+
+$this->addHookBefore('Pages::moved', function () {
   $http = new WireHttp();
   $response = $http->get("http://reverse-proxy:8080/api/purge-cache");
   if ($response !== false) {
