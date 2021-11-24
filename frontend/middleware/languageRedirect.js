b3a5d8da5e2adcb197bbe1949afcb885f12a28f4
PTRRR <pietro.alberti@bluewin.ch>
2021-11-24 10:16:31 +0100

    Fix safari language parsing

--- frontend/middleware/languageRedirect.js
+++ frontend/middleware/languageRedirect.js
@@ -5,15 +5,26 @@ export default function ({ app, ssrContext, route, params, redirect }) {
   const userLang = app.$cookiz.get('lang')
   if (!userLang) {
     const acceptLanguage = ssrContext?.req?.headers['accept-language']
-    const browserLanguage = parser.pick(['fr', 'en'], acceptLanguage)
-    app.$cookiz.set('lang', browserLanguage)
+    const languages = parser.parse(acceptLanguage)
 
-    const path = route.fullPath
-      .split('/')
-      .filter((it) => it !== params.language)
-      .join('/')
+    const { code: browserLanguage } = languages.reduce((acc, it) => {
+      if (!acc || acc.quality <= it.quality) {
+        acc = it
+      }
 
-    const url = getUrl(path, browserLanguage)
-    return redirect(url)
+      return it
+    }, null)
+
+    if (browserLanguage) {
+      app.$cookiz.set('lang', browserLanguage)
+
+      const path = route.fullPath
+        .split('/')
+        .filter((it) => it !== params.language)
+        .join('/')
+
+      const url = getUrl(path, browserLanguage)
+      return redirect(url)
+    }
   }
 }
