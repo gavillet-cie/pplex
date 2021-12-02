c53bf155cd4554b65f76771b9a994b650aca21be
PTRRR <pietro.alberti@bluewin.ch>
2021-12-02 10:44:44 +0100

    Update production url

--- frontend/config/production.js
+++ frontend/config/production.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL:
     process.env.VUE_ENV === 'server'
       ? 'http://cms/api'
-      : 'https://pplex-beta.gavillet-cie.com/api',
+      : 'http://192.168.100.22:8080/api',
 
-  ROOT_URL: 'https://pplex-beta.gavillet-cie.com',
+  ROOT_URL: 'http://192.168.100.22:8080',
 }
