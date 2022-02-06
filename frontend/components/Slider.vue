182792305a21454ff273ec0ef8b38b503492e8b1
PTRRR <pietro.alberti@bluewin.ch>
2022-02-06 23:31:46 +0100

    Improve slider

--- frontend/components/Slider.vue
+++ frontend/components/Slider.vue
@@ -2,6 +2,7 @@
   transition-group.slider(
     tag="div"
     name="slider"
+
     @click.native="onClick"
   )
     .slider__slide(
@@ -9,10 +10,8 @@
       :key="slideIndex"
     )
       span.slider__title {{ formatRawText(slide.title) }}
-      img(
-        :src="getImageUrl(slide.image.url)"
-        :style="getImageCssStyle(slide.image)"
-        :alt="slide.description || ''"
+      .slider__image(
+        :style="getSliderImageCssStyle(slide.image)"
       )
 </template>
 
@@ -32,6 +31,8 @@ export default {
   data() {
     return {
       slideIndex: 0,
+      isSliding: false,
+      interval: null,
     }
   },
 
@@ -39,12 +40,16 @@ export default {
     currentSlides() {
       return [this.slides[this.slideIndex]].filter((it) => it)
     },
+
+    sliderCssClasses() {
+      return {
+        'slider--is-sliding': this.isSliding,
+      }
+    },
   },
 
   mounted() {
-    setInterval(() => {
-      this.nextSlide()
-    }, 5000)
+    this.resetInterval()
   },
 
   methods: {
@@ -53,14 +58,34 @@ export default {
     formatRawText,
     formatHtmlText,
 
+    resetInterval() {
+      clearInterval(this.interval)
+      this.interval = setInterval(() => {
+        this.nextSlide()
+      }, 7000)
+    },
+
+    getSliderImageCssStyle(image) {
+      return {
+        backgroundImage: `url(${getImageUrl(image?.url)})`,
+      }
+    },
+
     nextSlide() {
       this.slideIndex++
       if (this.slideIndex > this.slides.length - 1) {
         this.slideIndex = 0
       }
+
+      this.isSliding = true
+      setTimeout(() => {
+        this.isSliding = false
+      }, 2000)
     },
 
     onClick() {
+      if (this.isSliding) return
+      this.resetInterval()
       this.nextSlide()
     },
   },
@@ -69,6 +94,8 @@ export default {
 
 <style lang="scss">
 .slider {
+  $s: &;
+
   user-select: none;
   position: relative;
   overflow: hidden;
@@ -78,11 +105,45 @@ export default {
 
   &-enter-active,
   &-leave-active {
-    transition: all 1s;
+    transition: all 1.5s 0.2s cubic-bezier(0.65, 0, 0.35, 1);
   }
-  &-enter,
+
+  &-enter-active {
+    position: relative;
+    left: initial !important;
+    right: 0 !important;
+
+    #{$s}__title {
+      transition: filter 1.8s 1s;
+    }
+
+    &::before {
+      opacity: 1 !important;
+    }
+  }
+
+  &-enter {
+    left: initial !important;
+    right: 0 !important;
+    width: 0% !important;
+
+    &::before {
+      opacity: 1 !important;
+    }
+
+    #{$s}__title {
+      filter: blur(20px);
+      opacity: 0;
+    }
+  }
+
   &-leave-to {
-    opacity: 0;
+    width: 0% !important;
+
+    #{$s}__title {
+      filter: blur(20px);
+      opacity: 0;
+    }
   }
 
   &__slide {
@@ -94,23 +155,42 @@ export default {
     overflow: hidden;
     clip: rect(0, 100vw, calc(100vh - 5rem), 0);
 
-    img {
-      width: 100%;
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: $menu-margin;
+      background-color: white;
       height: 100%;
-      object-fit: cover;
+      opacity: 0;
+      transition: opacity 0.2s;
     }
+  }
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    background-size: 100vw cover;
+    background-position: 50% 50%;
   }
 
   &__title {
     color: white;
     font-size: 8rem;
     position: fixed;
+    z-index: 10;
     top: calc(50vh + ((1 - var(--scroll-percent)) * 16.5vh));
     left: 50%;
     width: 80%;
     text-align: center;
     transform: translate(-50%, -50%);
     line-height: 1;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: '';
+    transition: filter 1.5s, opacity 1s;
   }
 
   @media screen and (max-width: 900px) {
