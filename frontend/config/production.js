6e10a4f066066dbb7303ad4d4a87b1480aec9c80
PTRRR <pietro.alberti@bluewin.ch>
2022-02-11 15:49:35 +0100

    Update to ssl config

--- frontend/config/production.js
+++ frontend/config/production.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL:
     process.env.VUE_ENV === 'server'
       ? 'http://cms/api'
-      : 'https://www.pplex.gavillet-cie.com/api',
+      : 'https://www.pplex.ch/api',
 
-  ROOT_URL: 'https://www.pplex.gavillet-cie.com',
+  ROOT_URL: 'https://www.pplex.ch',
 }
