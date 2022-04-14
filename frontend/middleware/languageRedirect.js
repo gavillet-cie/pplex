487a5e371e39984c8bfdf9b15a63622268c3de87
PTRRR <pietro.alberti@bluewin.ch>
2022-04-14 11:33:26 +0200

    Log request

--- frontend/middleware/languageRedirect.js
+++ frontend/middleware/languageRedirect.js
@@ -1,30 +1,32 @@
-import parser from 'accept-language-parser'
-import { getUrl } from '../utils/api'
+// import parser from 'accept-language-parser'
+// import { getUrl } from '../utils/api'
 
 export default function ({ app, ssrContext, route, params, redirect }) {
-  const userLang = app.$cookiz.get('lang')
-  if (!userLang) {
-    const acceptLanguage = ssrContext?.req?.headers['accept-language']
-    const languages = parser.parse(acceptLanguage)
+  // const userLang = app.$cookiz.get('lang')
+  console.log(ssrContext?.req)
+  console.log(ssrContext?.req?.headers)
+  // if (!userLang) {
+  //   const acceptLanguage = ssrContext?.req?.headers['accept-language']
+  //   const languages = parser.parse(acceptLanguage)
 
-    const { code: browserLanguage } = languages.reduce((acc, it) => {
-      if (!acc || acc.quality <= it.quality) {
-        acc = it
-      }
+  //   const { code: browserLanguage } = languages.reduce((acc, it) => {
+  //     if (!acc || acc.quality <= it.quality) {
+  //       acc = it
+  //     }
 
-      return it
-    }, null)
+  //     return it
+  //   }, null)
 
-    if (browserLanguage) {
-      app.$cookiz.set('lang', browserLanguage)
+  //   if (browserLanguage) {
+  //     app.$cookiz.set('lang', browserLanguage)
 
-      const path = route.fullPath
-        .split('/')
-        .filter((it) => it !== params.language)
-        .join('/')
+  //     const path = route.fullPath
+  //       .split('/')
+  //       .filter((it) => it !== params.language)
+  //       .join('/')
 
-      const url = getUrl(path, browserLanguage)
-      return redirect(url)
-    }
-  }
+  //     const url = getUrl(path, browserLanguage)
+  //     return redirect(url)
+  //   }
+  // }
 }
