3f45df9829d16947f9de85feaf63e10eb6c09fa1
PTRRR <pietro.alberti@bluewin.ch>
2022-04-14 11:53:40 +0200

    Cancel language redirection

--- frontend/middleware/languageRedirect.js
+++ frontend/middleware/languageRedirect.js
@@ -1,42 +1,39 @@
-// import parser from 'accept-language-parser'
-// import { getUrl } from '../utils/api'
+import parser from 'accept-language-parser'
+import { getUrl } from '../utils/api'
 
-const isGoogle = (headers) => {
+const cancelRedirection = (headers) => {
   return (
+    !(headers || {})['accept-language'] ||
     (headers || {}).from?.toLowerCase().includes('google') ||
     (headers || {})['user-agent'].toLowerCase().includes('google')
   )
 }
 
 export default function ({ app, ssrContext, route, params, redirect }) {
-  // const userLang = app.$cookiz.get('lang')
-  if (isGoogle(ssrContext?.req?.headers)) {
-    console.log('Google detected')
-  } else {
-    console.log('normal visitor')
+  if (cancelRedirection(ssrContext?.req?.headers)) return
+  const userLang = app.$cookiz.get('lang')
+  if (!userLang) {
+    const acceptLanguage = ssrContext?.req?.headers['accept-language']
+    const languages = parser.parse(acceptLanguage)
+
+    const { code: browserLanguage } = languages.reduce((acc, it) => {
+      if (!acc || acc.quality <= it.quality) {
+        acc = it
+      }
+
+      return it
+    }, null)
+
+    if (browserLanguage) {
+      app.$cookiz.set('lang', browserLanguage)
+
+      const path = route.fullPath
+        .split('/')
+        .filter((it) => it !== params.language)
+        .join('/')
+
+      const url = getUrl(path, browserLanguage)
+      return redirect(url)
+    }
   }
-  // if (!userLang) {
-  //   const acceptLanguage = ssrContext?.req?.headers['accept-language']
-  //   const languages = parser.parse(acceptLanguage)
-
-  //   const { code: browserLanguage } = languages.reduce((acc, it) => {
-  //     if (!acc || acc.quality <= it.quality) {
-  //       acc = it
-  //     }
-
-  //     return it
-  //   }, null)
-
-  //   if (browserLanguage) {
-  //     app.$cookiz.set('lang', browserLanguage)
-
-  //     const path = route.fullPath
-  //       .split('/')
-  //       .filter((it) => it !== params.language)
-  //       .join('/')
-
-  //     const url = getUrl(path, browserLanguage)
-  //     return redirect(url)
-  //   }
-  // }
 }
