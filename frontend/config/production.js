cec29742cd254ff30b0892a9c9282a2c12213773
PTRRR <pietro.alberti@bluewin.ch>
2022-04-14 12:06:52 +0200

    Restore production urls

--- frontend/config/production.js
+++ frontend/config/production.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL:
     process.env.VUE_ENV === 'server'
       ? 'http://cms/api'
-      : 'https://pplex.gavillet-cie.com/api',
+      : 'https://pplex.ch/api',
 
-  ROOT_URL: 'https://pplex.gavillet-cie.com',
+  ROOT_URL: 'https://pplex.ch',
 }
