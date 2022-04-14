d3352fd0228801c4a6480ea36d1d3d65e5f3c2a5
PTRRR <pietro.alberti@bluewin.ch>
2022-04-14 10:52:38 +0200

    Change to debug url

--- frontend/config/production.js
+++ frontend/config/production.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL:
     process.env.VUE_ENV === 'server'
       ? 'http://cms/api'
-      : 'https://pplex.ch/api',
+      : 'https://pplex.gavillet-cie.com/api',
 
-  ROOT_URL: 'https://pplex.ch',
+  ROOT_URL: 'https://pplex.gavillet-cie.com',
 }
