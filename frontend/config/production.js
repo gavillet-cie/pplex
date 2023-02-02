451a80d4338222c4f09bf8c1af49aa82cbb6d4ff
PTRRR <pietro.alberti@bluewin.ch>
2023-02-02 15:33:59 +0100

    Update config

--- frontend/config/production.js
+++ frontend/config/production.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL:
     process.env.VUE_ENV === 'server'
       ? 'http://cms/api'
-      : 'https://pplex.ch/api',
+      : 'https://www.pplex.ch/api',
 
-  ROOT_URL: 'https://pplex.ch',
+  ROOT_URL: 'https://www.pplex.ch',
 }
